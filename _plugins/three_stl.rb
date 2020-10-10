07dd1f17d78447f90e281fd9af33535eb5f05190
Tomoyuki Sakurai <y@trombik.org>
2020-10-10 17:14:29 +0900

    imp: create Player class to show an object in STL file

--- _plugins/three_stl.rb
+++ _plugins/three_stl.rb
@@ -23,14 +23,14 @@ module Jekyll
         <h2>Three</h2>
         <div id="#{div_id}"></div>
         <script type="module">
-          import * as THREE_STL from '/assets/js/three_stl.js';
+          import * as THREE_STL from '/assets/js/three_stl.js'
           var player_#{sha1(@stl_url)}
           player_#{sha1(@stl_url)} = new THREE_STL.Player()
           player_#{sha1(@stl_url)}.init({
             url: "#{@stl_url}",
             canvas_id: "#{div_id}",
           })
-          player_#{sha1(@stl_url)}.animate();
+          player_#{sha1(@stl_url)}.animate()
         </script>
         HTML
       end
