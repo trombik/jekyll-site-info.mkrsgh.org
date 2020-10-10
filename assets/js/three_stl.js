8005805472c57e8bd37bd8390e3be446c50da838
Tomoyuki Sakurai <y@trombik.org>
2020-10-10 17:14:29 +0900

    imp: create Player class to show an object in STL file

--- assets/js/three_stl.js
+++ assets/js/three_stl.js
@@ -2,152 +2,185 @@ import * as THREE from 'https://unpkg.com/three@0.121.1/build/three.module.js'
 import { STLLoader } from 'https://unpkg.com/three@0.121.1/examples/jsm/loaders/STLLoader.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.121.1/examples/jsm/controls/OrbitControls.js'
 
-var Player = function() {
-  var private_member, another_private_member
+var Player = function () {
   var container
-  var camera, cameraTarget, scene, renderer, control
-  var canvas, boxSize
+  var camera, scene, renderer, control
   var aspect = 4 / 3
 
   return {
     init: init,
-    animate: animate,
+    animate: animate
   }
 
-  function init(args) {
-    /* create the container that holds the scene */
-    container = document.createElement('div')
-
-    /* append the container to the given canvas */
-    canvas = document.getElementById(args.canvas_id)
-    canvas.appendChild(container)
-
-    /* create a scene */
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x292b2c)
-
-    /* load the STL file from URL */
+  function _loadSTL (url) {
     var loader = new STLLoader()
-    loader.load(args.url, function (geometry) {
-      /* called when onLoad event */
-      var material = new THREE.MeshPhongMaterial({ color: 0x0275d8, specular: 0x010101, shininess: 200})
-      var mesh = new THREE.Mesh(geometry, material)
+    return loader.loadAsync(url)
+  }
 
+  function _createMaterial () {
+    return new Promise(function (resolve, reject) {
+      var material = new THREE.MeshPhongMaterial({ color: 0x0275d8, specular: 0x010101, shininess: 200 })
+      resolve(material)
+    })
+  }
+
+  function _createMesh (url) {
+    var promises = [
+      _loadSTL(url),
+      _createMaterial()
+    ]
+    return Promise.all(promises).then((result) => {
+      var geometry = result[0]
+      var material = result[1]
+      var mesh = new THREE.Mesh(geometry, material)
       mesh.rotation.set(0, 0, 0)
       mesh.scale.set(1, 1, 1)
       mesh.castShadow = true
       mesh.receiveShadow = true
-      mesh.rotation.set( - Math.PI / 2, 0, 0 );
+      mesh.rotation.set(-Math.PI / 2, 0, 0)
       mesh.geometry.center()
-
-      /* create a bounding box of the object in the STL */
-      var box = new THREE.Box3().setFromObject(mesh)
-      boxSize = new THREE.Vector3()
-      box.getSize(boxSize)
-
-      mesh.position.set(0, boxSize.y / 2, 0)
-      box = new THREE.Box3().setFromObject(mesh)
-
-      /* create a ground */
-      var plane_size;
-      plane_size = Math.max(boxSize.x, boxSize.z) * 4
-      var plane = new THREE.Mesh(
-        new THREE.PlaneBufferGeometry(plane_size, plane_size),
-        new THREE.MeshPhongMaterial({ color: 0x444444, specular: 0x505050 })
-      )
-      plane.rotation.x = -Math.PI / 2
-      plane.position.y = 0
-      plane.receiveShadow = true
-      scene.add(plane)
-
-      /* add a grid */
-      var gridHelper = new THREE.GridHelper(plane_size, (plane_size / 10) - 1);
-      scene.add(gridHelper)
-
-      /* create a camera */
-      camera = new THREE.PerspectiveCamera(35, aspect, 1, 10000)
-
-      /* create the camera only when loading finishes because camera position
-       * depends on the size of the object */
-      var x = Math.max(boxSize.x, boxSize.z) * 3
-      var y = x
-      var z = x
-      camera.position.set(x, y, z)
-      cameraTarget = new THREE.Vector3(0, 0, 0)
-
-      /* create orbit controls */
-      control = new OrbitControls(camera, renderer.domElement)
-
-      scene.add(mesh)
-    }, undefined, function (err) {
-      /* called when onError event */
-      console.error(err)
+      return mesh
     })
+  }
 
-    /* create lights */
-    scene.add(new THREE.HemisphereLight(0x443333, 0x111122, 5))
-    _addShadowedLight(0.5, 1, -1, 0xffaa00, 1)
+  function _getBindingBoxSize (mesh) {
+    const box = new THREE.Box3().setFromObject(mesh)
+    var boxSize = new THREE.Vector3()
+    box.getSize(boxSize)
+    return boxSize
+  }
 
-    /* create a renderer */
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setPixelRatio(window.devicePixelRatio)
+  // create the container that holds the scene in `document`
+  function _createContainer (id) {
+    return new Promise(function (resolve, reject) {
+      var container = document.getElementById(id)
+      container.appendChild(document.createElement('div'))
+      resolve(container)
+    })
+  }
 
-    /* calculate the height from width of canvas.
-       *
-       * when initially loaded, canvas is empty. it has width, but height is
-       * zero.
-       */
-    renderer.setSize(canvas.offsetWidth, canvas.offsetWidth / aspect)
-    renderer.outputEncoding = THREE.sRGBEncoding
-    renderer.shadowMap.enabled = true
+  function _createGround (boxSize) {
+    const vector = boxSize
+    const groundSize = Math.max(vector.x, vector.z) * 4
+    var ground = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(groundSize, groundSize),
+      new THREE.MeshPhongMaterial({ color: 0x444444, specular: 0x505050 })
+    )
+    ground.rotation.x = -Math.PI / 2
+    ground.position.y = 0
+    ground.receiveShadow = true
+    return ground
+  }
 
-    /* finally, add the domElement of WebGL to the container */
-    container.appendChild(renderer.domElement)
+  // add a grid that fits to the given ground
+  function _createGrid (boxSize) {
+    var size = Math.max(boxSize.x, boxSize.z) * 4
+    return new THREE.GridHelper(size, size / 10)
+  }
 
-    /* add a listener to resize the canvas when window is resized */
-    window.addEventListener('resize', _onWindowResize, false)
+  /* create orbit controls */
+  function _createControl (camera, renderer) {
+    return new OrbitControls(camera, renderer.domElement)
+  }
+  // create a camera that locates at same distance and angle from the object
+  function _createCamera (boxSize) {
+    var aspect = 4 / 3
+    var camera = new THREE.PerspectiveCamera(35, aspect, 1, 1000)
+
+    // depends on the size of the object
+    var x = Math.max(boxSize.x, boxSize.z) * 3
+    var y = x
+    var z = x
+    camera.position.set(x, y, z)
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
+    return camera
+  }
+  // create light
+  function _createLights () {
+    return new THREE.HemisphereLight(0x443333, 0x111122, 5)
+  }
+
+  // create an WebGL renderer
+  function _createRenderer () {
+    return new THREE.WebGLRenderer({ antialias: true })
+  }
+
+  function _createScene (url) {
+    return new Promise(function (resolve, reject) {
+      _createMesh(url).then(function (mesh) {
+        var size = _getBindingBoxSize(mesh)
+        mesh.position.y += size.y / 2
+        var ground = _createGround(size)
+        var grid = _createGrid(size)
+        camera = _createCamera(size)
+        var light = _createLights()
+        renderer = _createRenderer()
+        control = _createControl(camera, renderer)
+        scene = new THREE.Scene()
+        scene.background = new THREE.Color(0x292b2c)
+        scene.add(ground)
+        scene.add(grid)
+        scene.add(camera)
+        scene.add(light)
+        scene.add(mesh)
+        resolve(scene)
+      })
+    })
+  }
+
+  function _init (args) {
+    var id = args.canvas_id
+    var url = args.url
+    var promises = [
+      _createContainer(id),
+      _createScene(url)
+    ]
+    return Promise.all(promises).then((result) => {
+      container = result[0]
+      scene = result[1]
+
+      /* calculate the height from width of container.
+         *
+         * when initially loaded, container is empty. it has width, but height is
+         * zero.
+         */
+      renderer.setSize(container.offsetWidth, container.offsetWidth / aspect)
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      renderer.shadowMap.enabled = true
+
+      /* finally, add the domElement of WebGL to the container */
+      container.appendChild(renderer.domElement)
+
+      /* add a listener to resize the container when window is resized */
+      window.addEventListener('resize', _updateSceneOnWindowResize, false)
+    })
+  }
+
+  function _updateSceneOnWindowResize () {
+    camera.updateProjectionMatrix()
+    control.update()
+    renderer.setSize(container.offsetWidth, container.offsetWidth / aspect)
+  }
+
+  function init (args) {
+    const promises = [
+      _init(args)
+    ]
+    return Promise.all(promises).then(result => {
+      return console.log('initialized')
+    })
   }
 
   function animate () {
     window.requestAnimationFrame(animate)
-    _render()
-  }
-
-  function _render () {
-    if (typeof camera !== 'undefined') {
-      camera.lookAt(cameraTarget)
+    if (typeof (camera) !== 'undefined' && typeof (scene) !== 'undefined' && typeof (renderer) !== 'undefined') {
+      camera.lookAt(new THREE.Vector3(0, 0, 0))
       renderer.render(scene, camera)
     }
-  }
-
-  function _doPrivateThings() {
-  }
-
-  function _addShadowedLight (x, y, z, color, intensity) {
-    var directionalLight = new THREE.DirectionalLight(color, intensity)
-    directionalLight.position.set(x, y, z)
-    scene.add(directionalLight)
-    directionalLight.castShadow = true
-
-    var d = 1
-    directionalLight.shadow.camera.left = -d
-    directionalLight.shadow.camera.right = d
-    directionalLight.shadow.camera.top = d
-    directionalLight.shadow.camera.bottom = -d
-
-    directionalLight.shadow.camera.near = 1
-    directionalLight.shadow.camera.far = 4
-
-    directionalLight.shadow.bias = -0.002
-  }
-
-  function _onWindowResize () {
-    camera.aspect = aspect
-    camera.updateProjectionMatrix()
-    control.update()
-    renderer.setSize(canvas.offsetWidth, canvas.offsetWidth / aspect)
   }
 }
 
 export { Player }
+
 // vim: shiftwidth=2 tabstop=2
